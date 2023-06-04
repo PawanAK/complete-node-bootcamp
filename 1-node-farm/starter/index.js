b0307fd0c5e62c9b54e8d5b51af3c539978ac16c
PawanAK <pawanajjark@gmail.com>
2023-06-04 20:24:56 +0530

    setup 2

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -5,4 +5,3 @@ const textIn = fs.readFileSync("./txt/input.txt", "utf8");
 const textOut = `This is not a great way to understand this is a great way to learn ${Date.now()}`;
 
 fs.writeFileSync("./txt/output.txt", textOut);
-console.log("file Written");
