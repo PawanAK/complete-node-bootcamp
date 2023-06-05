91bf9dcb5b5aa597c3aea4c00ac65e41083dd549
PawanAK <pawanajjark@gmail.com>
2023-06-05 22:17:14 +0530

    http

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,17 +1,6 @@
 const fs = require("fs");
+const http = require("http");
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf8", (err, data2) => {
-    console.log(data2);
-
-    fs.readFile(`./txt/append.txt`, "utf8", (err, data3) => {
-      console.log(data3);
-
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log(err);
-      });
-    });
-  });
+http.createServer((req, res) => {
+  res.end("Hello from server");
 });
-
-console.log("reading file");
