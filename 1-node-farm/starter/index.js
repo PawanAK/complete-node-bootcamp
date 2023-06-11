27d852543baa7b863063bd90ad78b711e9590b64
PawanAK <pawanajjark@gmail.com>
2023-06-11 21:26:41 +0530

    Overview Page

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,20 +2,42 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  //OVERVIEW PAGE
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the overview");
-  } else if (pathName === "/product") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end(tempOverview);
+  }
+  //Product page
+  else if (pathName === "/product") {
     res.end("this is the Product");
-  } else if (pathName === "/api") {
+  }
+  //API
+  else if (pathName === "/api") {
     res.writeHead(200, { "content-type": "application/json" });
     res.end(data);
-  } else {
+  }
+  //Not found
+  else {
     res.writeHead(404, {
       "Content-type": "text/html",
     });
