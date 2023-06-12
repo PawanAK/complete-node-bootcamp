16473696982cb7b7d9c2c6ea6d469f1ccce5ab21
PawanAK <pawanajjark@gmail.com>
2023-06-12 21:31:43 +0530

    pages

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -37,10 +37,12 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+  console.log(query);
+  console.log(pathname);
 
   //OVERVIEW PAGE
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "content-type": "text/html" });
 
     const cardsHtml = dataObj
@@ -51,11 +53,11 @@ const server = http.createServer((req, res) => {
     res.end(output);
   }
   //Product page
-  else if (pathName === "/product") {
+  else if (pathname === "/product") {
     res.end("this is the Product");
   }
   //API
-  else if (pathName === "/api") {
+  else if (pathname === "/api") {
     res.writeHead(200, { "content-type": "application/json" });
     res.end(data);
   }
