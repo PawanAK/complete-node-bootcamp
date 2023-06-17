d036baa67f65556f177c950f7ca6b0650e860775
PawanAK <pawanajjark@gmail.com>
2023-06-17 21:30:19 +0530

    readable.pipe

--- 2-how-node-works/starter/streams.js
+++ 2-how-node-works/starter/streams.js
@@ -3,10 +3,8 @@ const fs = require("fs");
 const server = require("http").createServer();
 
 server.on("request", (req, res) => {
-  fs.readFile("test-file.txt", (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
-  });
+  const readable = fs.createReadStream("test-file.txt");
+  readable.pipe(res);
 });
 
 server.listen(8000, "127.0.0.1", () => {
