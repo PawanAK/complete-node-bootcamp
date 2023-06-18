13a75a3df18f59177f30d133d520847f64ae9ed9
PawanAK <pawanajjark@gmail.com>
2023-06-18 18:42:06 +0530

    Promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,12 +1,23 @@
-const fs = require("fs");
-const superagent = require("superagent");
+const fs = require('fs');
+const superagent = require('superagent');
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {});
+};
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed:${data}`);
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      console.log(res.body);
+    .then((res) => {
+      console.log(res.body.message);
+
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        console.log('rondom dog image saved to file');
+      });
+    })
+    .catch((err) => {
+      console.log(err).message;
     });
 });
