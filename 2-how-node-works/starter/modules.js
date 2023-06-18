b2cf57524b038eb5741dc3f21b076a07a2069ad3
PawanAK <pawanajjark@gmail.com>
2023-06-18 08:48:56 +0530

    alternative export

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -1,4 +1,7 @@
-const C = require("./test-module-1");
-const calc1 = new C();
+// const C = require("./test-module-1");
+// const calc1 = new C();
 
-console.log(calc1.add(2, 5));
+// console.log(calc1.add(2, 5));
+
+const { add, multiply } = require("./test-module-2");
+console.log(multiply(2, 5));
