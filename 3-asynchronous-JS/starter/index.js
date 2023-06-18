42dbce68290e06b97d13b92bdd57d7b2d087c44d
PawanAK <pawanajjark@gmail.com>
2023-06-18 19:40:06 +0530

    custom promise

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -2,10 +2,15 @@ const fs = require('fs');
 const superagent = require('superagent');
 
 const readFilePro = (file) => {
-  return new Promise((resolve, reject) => {});
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('File not found');
+      resolve(data);
+    });
+  });
 };
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+readFilePro(`${__dirname}/dog.txt`).then((data) => {
   console.log(`Breed:${data}`);
 
   superagent
