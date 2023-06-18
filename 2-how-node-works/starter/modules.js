8b0d47ac809027014fbb26d0a8122bd554a344e5
PawanAK <pawanajjark@gmail.com>
2023-06-18 11:32:30 +0530

    cachin

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -1,7 +1,3 @@
-// const C = require("./test-module-1");
-// const calc1 = new C();
-
-// console.log(calc1.add(2, 5));
-
-const { add, multiply } = require("./test-module-2");
-console.log(multiply(2, 5));
+require("./test-module-3")();
+require("./test-module-3")();
+require("./test-module-3")();
