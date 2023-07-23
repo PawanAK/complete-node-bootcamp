05f77ff5735b2d15aa360066d35e03a6084dff8c
PawanAK <pawanajjark@gmail.com>
2023-07-23 20:07:38 +0530

    ..

---
+++ 4-natours/starter/routes/tourRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router()
+
+app.use('/api/v1/tours', router)
+
+router.route('/').get(getAllTours).post(createTour);
+
+router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+module.exports = router;
